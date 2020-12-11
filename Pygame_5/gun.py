228143f47d66dcb004b3e52bc8c554884c9b6390
ChekalMikhail <mishka.chekal@gmail.com>
2020-12-11 10:24:02 +0300

    added solar system

--- Pygame_5/gun.py
+++ Pygame_5/gun.py
@@ -1,10 +1,13 @@
 from random import randrange as rnd, choice
+import csv
 import tkinter as tk
+from tkinter import *
 import math
 import time
 import copy
 
 # print (dir(math))
+
 
 root = tk.Tk()
 fr = tk.Frame(root)
@@ -13,6 +16,8 @@ canv = tk.Canvas(root, bg='white')
 canv.pack(fill=tk.BOTH, expand=1)
 timer = 0
 
+'''ent = Entry(root, width=100)
+lbl = Label(root, width=100)'''
 
 class ball():
     def __init__(self, x=40, y=450):
@@ -66,28 +71,6 @@ class ball():
         if self.y > 590 or self.y < 10:
             self.vy = -self.vy
             self.vx /= 2
-        '''if self.reduce_x_speed:
-            self.x_timer += 1
-        if self.reduce_x_speed:
-            self.y_timer += 1
-        self.x += self.vx
-        self.y -= self.vy
-        if self.x > 790 or self.x < 10:
-            self.reduce_x_speed = True
-            self.x_timer = 0
-            self.vx = -self.vx
-        if self.y > 590 or self.y < 10:
-            self.reduce_y_speed = True
-            self.y_timer = 0
-            self.vy = -self.vy
-        elif self.reduce_x_speed and self.x_timer >= 3:
-            self.vx *= .1
-            self.reduce_x_speed = False
-            self.x_timer = 0
-        elif self.reduce_y_speed and self.y_timer >= 3:
-            self.vx *= .1
-            self.reduce_x_speed = False
-            self.x_timer = 0'''
 
         self.vy -= 2
         canv.coords(
@@ -156,8 +139,8 @@ class gun():
 
     def power_up(self):
         if self.f2_on:
-            if self.f2_power < 40:
-                self.f2_power += 1
+            if self.f2_power < 20:
+                self.f2_power += 5
             canv.itemconfig(self.id, fill='orange')
         else:
             canv.itemconfig(self.id, fill='black')
@@ -165,8 +148,8 @@ class gun():
 
 class target():
     def __init__(self):
-        self.color = 'red'
-        self.r = rnd(60, 80)
+        self.color = choice(['blue', 'green', 'red', 'yellow'])
+        self.r = rnd(10, 50)
         self.x = rnd(610, 770)
         self.y = rnd(310, 540)
         self.vx = rnd(1, 2)
@@ -181,9 +164,9 @@ class target():
         self.x -= self.vx
         self.y -= self.vy
 
-        if self.x < 600 or self.x > 780:
+        if self.x < 300 + self.r or self.x > 800 - self.r:
             self.vx = -self.vx
-        if self.y < 300 or self.y > 550:
+        if self.y < 300 + self.r or self.y > 550 - self.r:
             self.vy = -self.vy
         canv.coords(self.id, self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
 
@@ -196,7 +179,10 @@ class target():
         """Попадание шарика в цель."""
         canv.coords(self.id, -10, -10, -10, -10)
         self.points += points
-        canv.itemconfig(self.id_points, text=self.points)
+        # l1 = Label(root, text="Счёт:" + str(self.points),
+        #          font="Arial 25")
+        # l1.pack()
+        # canv.itemconfig(self.id_points, text=self.points)
 
     def teleport(self):
         self.x = rnd(610, 770)
@@ -206,58 +192,139 @@ class target():
         self.vy = rnd(1, 2)
 
 
-t1 = target()
+# t1 = target()
 screen1 = canv.create_text(400, 300, text='', font='28')
 g1 = gun()
 bullet = 0
 balls = []
-'''targets = []
-for _ in range(2):
-    targets.append(target())'''
+targets = []
+for _ in range(5):
+    targets.append(target())
+
+message = StringVar()
+message_entry = Entry(textvariable=message)
+message_entry.pack()
+message_entry.place(relx=.5, rely=.1, anchor="c")
+
+
+def table_of_leaders():
+    message_entry.insert(0, "")
+    username = message.get()
+    print("<"+username+">")
+    with open('best_scores.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow([username])
+
+
+class ExitButton:
+    def __init__(self):
+        self.b = Button(text='Выход', width=10, height=3, font="Arial 10")
+        self.b.bind('<Button-1>', self.exit_, table_of_leaders())
+        self.b.pack()
+
+    def change(self, event):
+        self.b['fg'] = "red"
+        self.b['activeforeground'] = "red"
+
+    def dialog(self, event):
+        root.configure(background='black')
+        canv.itemconfig(screen1, text="Ваш счет: {}")
+
+    def exit_(self, event):
+        root.destroy()
+
+
+ExitButton()
+
+'''def score():
+    canv.itemconfig(screen1, text="Счет : {}")
+    # text_1 = font.render("Счет : {}".format(count), True, WHITE)
+    # surface.blit(text_1, (0, 0))'''
+
+'''
+def user_text(surface, count):
+    global username
+    font = pygame.font.SysFont('arial', 32, True)
+    text_1 = font.render(username, True, WHITE)
+    surface.blit(text_1, text_1.get_rect(center=(width / 2, height / 2)))
+    pygame.display.update()
+    for events in pygame.event.get():
+        if events.type == pygame.KEYDOWN:
+            if events.key == pygame.K_BACKSPACE:
+                username = username[:-1]
+            elif events.key == pygame.K_RETURN:
+                if username != "":
+                    with open('best_scores.csv', 'a') as file:
+                        writer = csv.writer(file)
+                        writer.writerow([username, count, time])
+                return True
+            else:
+                username += events.unicode'''
+
+
+
+
+
+
 
 
 def new_game(event=''):
-    global gun, t1, screen1, balls, bullet, timer
-    t1.new_target()
+    global gun, targets, screen1, balls, bullet, timer
+    count = 0
+
+
+
+    for i in targets:
+        i.new_target()
+        i.live = 1
     bullet = 0
     balls = []
     canv.bind('<Button-1>', g1.fire2_start)
     canv.bind('<ButtonRelease-1>', g1.fire2_end)
     canv.bind('<Motion>', g1.targetting)
+    """canv.bind('<Escape>', dialog)"""
+    while True:
+        for i in targets:
+            for b in balls:
+                b.move()
+                if b.hittest(i) and i.live and timer > 30:
+                    this_bullet = str(copy.copy(bullet))
+                    i.live = 0
+                    i.hit()
+                    canv.bind('<Button-1>', '')
+                    canv.bind('<ButtonRelease-1>', '')
+                    canv.itemconfig(screen1, text='Вы уничтожили цель за ' + this_bullet + ' выстрелов')
+                    count += 1
+                    timer = 0
+                    bullet = 0
 
-    z = 0.03
-    t1.live = 1
-    while t1.live or balls:
-        for b in balls:
-            b.move()
-            if b.hittest(t1) and t1.live and timer > 30:
-                this_bullet = str(copy.copy(bullet))
-                t1.live = 0
-                t1.hit()
-                canv.bind('<Button-1>', '')
-                canv.bind('<ButtonRelease-1>', '')
-                canv.itemconfig(screen1, text='Вы уничтожили цель за ' + this_bullet + ' выстрелов')
-                timer = 0
-                bullet = 0
+                    i.teleport()
+                    i.live = 1
+                    canv.bind('<Button-1>', g1.fire2_start)
+                    canv.bind('<ButtonRelease-1>', g1.fire2_end)
+                    canv.bind('<Motion>', g1.targetting)
 
-                t1.teleport()
-                t1.live = 1
-                canv.bind('<Button-1>', g1.fire2_start)
-                canv.bind('<ButtonRelease-1>', g1.fire2_end)
-                canv.bind('<Motion>', g1.targetting)
-
-        canv.update()
-        time.sleep(0.03)
-        g1.targetting()
-        g1.power_up()
-        t1.move_target()
-        timer += 1
-        if timer >= 40:
-            canv.itemconfig(screen1, text='')
+            canv.update()
+            time.sleep(0.003)
+            g1.targetting()
+            g1.power_up()
+            for i in targets:
+                i.move_target()
+            timer += 1
+            if timer >= 40:
+                canv.itemconfig(screen1, text='')
     canv.itemconfig(screen1, text='')
     canv.delete(gun)
     root.after(750, new_game)
+    return count
 
+l1 = Label(root, text="Счёт:" + str(new_game()),
+           font="Arial 25")
+# l1.config(bd=5, bg='#ffaaaa')
+l1.place(relx=0.0,
+         rely=1.0,
+         anchor='sw')
+l1.pack()
 
 new_game()
 root.mainloop()
